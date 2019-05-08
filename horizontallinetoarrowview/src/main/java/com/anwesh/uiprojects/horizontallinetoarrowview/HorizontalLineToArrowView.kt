d63f1aa5399bdb99686aa13050bf09ee18d6c0fd
Anwesh43 <anweshthecool0@gmail.com>
2019-05-08 09:36:35 +0530

    adding Int and Float extension methods

--- horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
+++ horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
@@ -21,3 +21,13 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#6A1B9A")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(scale : Float, i : Int, n : Int) : Float = Math.max(0f, scale - i * n.inverse())
+fun Float.divideScale(scale : Float, i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(scale, i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
