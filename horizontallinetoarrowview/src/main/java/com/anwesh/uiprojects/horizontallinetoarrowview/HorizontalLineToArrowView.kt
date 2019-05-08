fe39dc44d0cf09c387cbba09dcf2896da9a543ac
Anwesh43 <anweshthecool0@gmail.com>
2019-05-08 10:25:46 +0530

    using renderer in view

--- horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
+++ horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
@@ -63,14 +63,17 @@ fun Canvas.drawHLTNode(i : Int, scale : Float, paint : Paint) {
 
 class HorizontalLineToArrowView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
