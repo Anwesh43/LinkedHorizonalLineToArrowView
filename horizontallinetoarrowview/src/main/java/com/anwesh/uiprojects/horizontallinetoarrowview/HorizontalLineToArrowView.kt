f7c664bb262e15c2f8d0e73b5677dc6c5ed2d27e
Anwesh43 <anweshthecool0@gmail.com>
2019-05-08 10:13:23 +0530

    adding HLTNode class

--- horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
+++ horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
@@ -119,8 +119,52 @@ class HorizontalLineToArrowView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class HLTNode(var i : Int, val state : State = State()) {
+
+        private var next : HLTNode? = null
+        private var prev : HLTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HLTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHLTNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HLTNode {
+            var curr : HLTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
