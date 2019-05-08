3503ae4f5f76ad3f2fc9a332ba999e688538d202
Anwesh43 <anweshthecool0@gmail.com>
2019-05-08 09:29:49 +0530

    adding libraries and constants

--- horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
+++ horizontallinetoarrowview/src/main/java/com/anwesh/uiprojects/horizontallinetoarrowview/HorizontalLineToArrowView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.horizontallinetoarrowview
 /**
  * Created by anweshmishra on 08/05/19.
  */
+
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#6A1B9A")
+val backColor : Int = Color.parseColor("#BDBDBD")
