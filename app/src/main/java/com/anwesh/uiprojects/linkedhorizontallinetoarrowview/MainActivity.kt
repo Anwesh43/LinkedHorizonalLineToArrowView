8fff520e24439452d74a7c01765a1289897288f0
Anwesh43 <anweshthecool0@gmail.com>
2019-05-08 10:32:23 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhorizontallinetoarrowview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhorizontallinetoarrowview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhorizontallinetoarrowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.horizontallinetoarrowview.HorizontalLineToArrowView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HorizontalLineToArrowView.create(this)
     }
 }
